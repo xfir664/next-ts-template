9875b9e4714de2a1d339bd9034eed3c5ca1e69dc
MamkinCoder <aleksey.aleksandrovich.mc@gmail.com>
2025-07-13 14:26:16 +0300

    start

---
+++ src/app/utils/localFonts.ts
@@ -0,0 +1,23 @@
+import localFont from "next/font/local";
+
+const ptSans = localFont({
+  src: [
+    {
+      path: "@/app/fonts/pt-sans/PTSans-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "@/app/fonts/pt-sans/PTSans-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "@/app/fonts/pt-sans/PTSans-Italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+  ],
+  variable: "--font-pt-sans",
+  display: "swap",
+});
